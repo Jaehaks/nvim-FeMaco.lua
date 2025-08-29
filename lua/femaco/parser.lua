ec0f733d622c488f2f81c0cae5f866d04ca1595c
Jaehaks <wogkr3654@naver.com>
2025-08-29 11:09:23 +0900

    feat(parser) : add get_childtext() to get contents of child node

--- lua/femaco/parser.lua
+++ lua/femaco/parser.lua
@@ -20,5 +20,42 @@ M.is_node = function(target)
 	return node
 end
 
+-- get targeted child node from parent node
+---@param node TSNode? parent node to get their child content
+---@param target string child node name which you want to get contents
+---@return TSNode? string child node name which you want to get contents
+local function get_child(node, target)
+	if not node then
+		return nil
+	end
+
+	if node:type() == target then
+		return node
+	end
+
+	for child in node:iter_children() do
+		local cnode = get_child(child, target)
+		if cnode then
+			return cnode
+		end
+	end
+
+	return nil
+end
+
+-- Get contents of child node which is target
+---@param node TSNode? Parent node to get their child content
+---@param target string Child node name which you want to get contents
+---@return string? Contents of child node
+M.get_childtext = function (node, target)
+	local child = get_child(node, target)
+
+	if child then
+		return vim.treesitter.get_node_text(child, 0)
+	else
+		return nil
+	end
+end
+
 return M
 
