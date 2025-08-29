3ec458d6e35597638a954f2218642c958c456a24
Jaehaks <wogkr3654@naver.com>
2025-08-29 10:38:20 +0900

    fix(parser) : fix bug for is_node()
    1. Purpose of is_node()
    
    To check whether the node under cursor is the same with target node
    It returns (boolean, TSNode?) it can be used to detect this node is
    fenced_code_block

--- lua/femaco/parser.lua
+++ lua/femaco/parser.lua
@@ -1,22 +1,23 @@
 local M = {}
 
----@param targets string|table target node to check this region under cursor is the node
+---@param target string target node name to check this region under cursor is the node
+--- @return boolean Whether the node which is same with target exists.
 --- @return TSNode? Object of treesitter tree
-M.is_node = function(targets)
+M.is_node = function(target)
 	local snode = vim.treesitter.get_node() -- get smallest node of current buffer and cursor
 	if not snode then
-		return nil
+		return false, nil
 	end
 
 	local node = nil
-	while node do
-		if snode:type() == targets then
+	while not node do
+		if snode:type() == target then
 			node = snode
 		end
-		node = node:parent()
+		snode = snode:parent()
 	end
 
-	return node
+	return true, node
 end
 
 return M
